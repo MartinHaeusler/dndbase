c485f927c69fcb41224a4d6b06afb6226f0684ec
Martin Häusler <martin.haeusler89@gmail.com>
2023-09-02 00:24:28 +0200

    Fix used protocol for REST calls on the client

--- client/src/api/server.ts
+++ client/src/api/server.ts
@@ -2,45 +2,30 @@ const SERVER_PORT = 8080;
 
 const SERVER_HOST = window.location.hostname;
 
-export const SERVER_URL_HTTP = `http://${SERVER_HOST}:${SERVER_PORT}`;
+const PROTOCOL = window.location.protocol;
+
+export const SERVER_URL_HTTP = `${PROTOCOL}//${SERVER_HOST}:${SERVER_PORT}`;
+
+console.log('PROTOCOL: ', PROTOCOL);
 
 export async function fetchGet(path: string, parameters: any = {}) {
-  return fetch(
-    `${SERVER_URL_HTTP}/${removePrefix(path, "/")}?${new URLSearchParams(
-      parameters
-    )}`,
-    {
-      method: "GET",
-    }
-  );
+  return fetch(`${SERVER_URL_HTTP}/${removePrefix(path, '/')}?${new URLSearchParams(parameters)}`, {
+    method: 'GET',
+  });
 }
 
-export async function fetchPost(
-  path: string,
-  body?: any,
-  parameters: any = {}
-) {
-  return fetch(
-    `${SERVER_URL_HTTP}/${removePrefix(path, "/")}?${new URLSearchParams(
-      parameters
-    )}`,
-    {
-      method: "POST",
-      body: body ? JSON.stringify(body) : undefined,
-    }
-  );
+export async function fetchPost(path: string, body?: any, parameters: any = {}) {
+  return fetch(`${SERVER_URL_HTTP}/${removePrefix(path, '/')}?${new URLSearchParams(parameters)}`, {
+    method: 'POST',
+    body: body ? JSON.stringify(body) : undefined,
+  });
 }
 
 export async function fetchPut(path: string, body?: any, parameters: any = {}) {
-  return fetch(
-    `${SERVER_URL_HTTP}/${removePrefix(path, "/")}?${new URLSearchParams(
-      parameters
-    )}`,
-    {
-      method: "PUT",
-      body: body ? JSON.stringify(body) : undefined,
-    }
-  );
+  return fetch(`${SERVER_URL_HTTP}/${removePrefix(path, '/')}?${new URLSearchParams(parameters)}`, {
+    method: 'PUT',
+    body: body ? JSON.stringify(body) : undefined,
+  });
 }
 
 function removePrefix(s: string, prefix: string): string {
